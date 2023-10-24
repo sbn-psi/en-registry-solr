97daaf5a2e80fbbec82d7a35a844afc0c2ad217c
Catherine Suh <csuh@jpl.nasa.gov>
2023-10-24 16:19:29 -0700

    Remove commented copyright statements

--- src/main/java/gov/nasa/pds/search/core/logging/ToolsLevel.java
+++ src/main/java/gov/nasa/pds/search/core/logging/ToolsLevel.java
@@ -1,15 +1,3 @@
-// Copyright 2006-2010, by the California Institute of Technology.
-// ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
-// Any commercial use must be negotiated with the Office of Technology Transfer
-// at the California Institute of Technology.
-//
-// This software is subject to U. S. export control laws and regulations
-// (22 C.F.R. 120-130 and 15 C.F.R. 730-774). To the extent that the software
-// is subject to U.S. export control laws and regulations, the recipient has
-// the responsibility to obtain export licenses or other export authority as
-// may be required before exporting such information to foreign countries or
-// providing access to foreign nationals.
-//
 // $Id: ToolsLevel.java 10558 2012-05-25 22:20:10Z mcayanan $
 package gov.nasa.pds.search.core.logging;
 
