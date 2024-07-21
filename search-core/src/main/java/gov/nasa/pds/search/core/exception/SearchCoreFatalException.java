3e902f3be4ef8153c5053dda9fd2daaea7355853
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2024-07-21 10:53:49 -0700

    Re-add search core back

---
+++ search-core/src/main/java/gov/nasa/pds/search/core/exception/SearchCoreFatalException.java
@@ -0,0 +1,11 @@
+package gov.nasa.pds.search.core.exception;
+
+public class SearchCoreFatalException extends Exception {
+
+  private static final long serialVersionUID = 6281907612825712196L;
+
+  public SearchCoreFatalException(String string) {
+    super(string);
+  }
+
+}
